77a410f93dfcc030eaa4fd739e5c9dc7e2e4001f
ABHI <mba384420@gmail.com>
2024-01-21 11:58:28 +0530

    start_screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
-
+import 'start_screen.dart';
 void main() {
-  runApp(MaterialApp(
+  runApp(
+       MaterialApp(
     home: Scaffold(
-      body:Text('quiz app')
+      body:Container(
+         decoration: const BoxDecoration(
+           gradient: LinearGradient(
+             colors: [Color.fromARGB(255, 78, 13,151),
+               Color.fromARGB(255, 107, 15, 168)],
+             begin: Alignment.topLeft,
+             end: Alignment.bottomRight
+           )
+         ),
+         child: const StartScreen(),
+      )
     ),
   ));
 }
