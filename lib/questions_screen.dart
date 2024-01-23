f62c759d22d857497eca5e9f901a2acaf78fbbaa
ABHI <mba384420@gmail.com>
2024-01-24 00:01:54 +0530

    all questions are added to the question screen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
+import 'data/questions.dart';
 import 'answer_button.dart';
+import 'models/quiz_question.dart';
 class QuestionsScreen extends StatefulWidget
 {
   const QuestionsScreen({super.key});
@@ -12,21 +14,42 @@ class QuestionsScreen extends StatefulWidget
 
 class _QuestionsScreenState extends State<QuestionsScreen>
 {
+  var currentQuestionIndex=0;
+  void answerQuestion()
+  {
+    setState(() {
+      currentQuestionIndex++;
+    });
+
+  }
+
   @override
   Widget build(context)
   {
-    return  SizedBox(
+    final currentQuestion=questions[currentQuestionIndex];
+    return   SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text('The question...'),
-          SizedBox(height: 30,),
-          AnswerButton('Answer1'),
-          AnswerButton('Answer2'),
-          AnswerButton('Answer3'),
+          Text(currentQuestion.text,
+          style: const TextStyle(
+            color: Colors.white,
+          ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 30,),
+          ...currentQuestion.answer.map((answer)
+          {
+            return AnswerButton(
+              answerText: answer,
+              onTap:answerQuestion,
+            );
+          }
+          )
         ],
       ),
-    ) ;
+    );
   }
 }
